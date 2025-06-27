0e24974b90d3135b3866f14d2416c54aef56c517
firoze-hossain <firoze.hossain@outlook.com>
2025-06-27 19:27:04 +0600

    first commit

---
+++ src/main/java/com/roze/devcanvas/DevcanvasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roze.devcanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcanvasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcanvasBackendApplication.class, args);
+	}
+
+}
